62911f0e074c2c296484606325690e81bc5878c3
Alx <Alx@ROG-GL553VE>
2021-03-02 10:38:05 +0200

    Week 2 before the break

---
+++ Seminar2/src/ro/ase/csie/cts/g1092/week2/test/TestApp.java
@@ -0,0 +1,13 @@
+package ro.ase.csie.cts.g1092.week2.test;
+
+import ro.ase.csie.cts.g1092.week2.interest.Profitable;
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Profitable.MAX_INTEREST_PERCENT = 10;
+
+	}
+
+}
